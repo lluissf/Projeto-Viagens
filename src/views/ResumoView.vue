3d08553fdaec3059f097fb32e7770ad44934d8a0
tom-locatelli47 <tomaslocatelliifc@gmail.com>
2025-07-11 16:37:46 -0300

    nome da pagina e icon

--- src/views/ResumoView.vue
+++ src/views/ResumoView.vue
@@ -1,10 +1,25 @@
 <script setup>
+import { useRouter } from 'vue-router'
+import { useCidade } from '@/stores/cidade'
+import { useMeioTransporte } from '@/stores/meio-transporte'
+import { useFormaPagamento } from '@/stores/forma-pagamento'
+
+const cidadeStore = useCidade()
+const pagamento = useFormaPagamento()
+const locomocaoStore = useMeioTransporte()
+const router = useRouter()
+
 
 </script>
 
 <template>
+
     <div> 
-        <h1>Resumo da compra.</h1>      
+        
+
+        <h1>Resumo da compra.</h1>    
         <p>Você chegou ao fim!</p>
+
     </div>
+           
 </template>
