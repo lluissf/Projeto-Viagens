743609e9484640164c853124bc361cb49b0e783a
Luis <luis.felipe.ferreira.da.silva.if@gmail.com>
2025-07-11 10:00:58 -0300

    Correção do Forma de Pagamento

--- src/views/FormaPagamentoView.vue
+++ src/views/FormaPagamentoView.vue
@@ -1,30 +1,23 @@
 <script setup>
- import { useRouter } from 'vue-router'
- import { useFormaPagamento } from '@/stores/forma-pagamento';
- pagamentoStore = useFormaPagamento
+import { useRouter } from 'vue-router'
+import { useFormaPagamento } from '@/stores/forma-pagamento'
 
- const router = useRouter()
-
- const meioSelecionado = toRef(pagamentoStore, 'meioSelecionado')
+const router = useRouter()
+const pagamentoStore = useFormaPagamento()
 </script>
 
 <template>
-<div> 
-  <template>  
-   <h1>Informe a forma que deseja realiazar o pagamento.</h1>
-    <select  v-model="meioSelecionado">
-       <option disabled value="">Escolha o meio de pagamento</option>
-       <option
-         v-for="meio in pagamentoStore.pagamento"
-         :key="meio"
-         :value="meio"
-        >
+  <div>
+    <h1>Informe a forma que deseja realizar o pagamento.</h1>
+    <select v-model="pagamentoStore.meioSelecionado">
+      <option disabled value="">Escolha o meio de pagamento</option>
+      <option v-for="meio in pagamentoStore.pagamento" :key="meio" :value="meio">
         {{ meio }}
-       </option>
-     </select>
-        <button><router-link to="/resumo">Finalizar Compra.</router-link></button>
-  
- </template>
-</div>
-     
+      </option>
+    </select>
+
+    <button @click="router.push('/resumo')" :disabled="!pagamentoStore.meioSelecionado">
+      Finalizar Compra
+    </button>
+  </div>
 </template>
