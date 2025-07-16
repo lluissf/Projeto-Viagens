d3e586f52c710f61d47d1c16c74fcb39f7b64e63
tom-locatelli47 <tomaslocatelliifc@gmail.com>
2025-07-16 08:33:22 -0300

    umas partes inuteis removidas

--- src/App.vue
+++ src/App.vue
@@ -36,22 +36,11 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
 nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 
